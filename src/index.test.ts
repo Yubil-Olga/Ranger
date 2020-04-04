c0caf36bbbc1fb4a402e390a1bb85ea4835af34f
Yubil-Olga <Yubil-Olga@yandex.ru>
2020-04-04 14:57:12 +0300

    new folder for slider, webpack config

--- src/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { sum } from './index'
-
-test('My first test', ( ) => {
-    expect(sum(1, 3)).toBe(4)
-    expect(sum(1, 30)).toBe(31)
-})
--- src/slider.d.ts
+++
@@ -1,4 +0,0 @@
-interface JQuery {
-    myPlugin: any;
-    perfectSlider: any;
-}
