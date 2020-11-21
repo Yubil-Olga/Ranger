0f28c416eaf4e303bfffb00d162ddceaf3684df9
Yubil-Olga <Yubil-Olga@yandex.ru>
2020-11-21 23:35:46 +0300

    tests #20

--- src/app/Model/Options/Options.test.ts
+++ src/app/Model/Options/Options.test.ts
@@ -1,95 +1,150 @@
 import Model from '../Model';
 
-const arr = [];
+describe('Options validation', () => {
+  const model = new Model({});
 
-arr[0] = {
-  options : {start: -25, end: 100, step: 25, scaleStep: 10, prefix: '%', color: 'red', to: 0},
-  result : {isRange: false, isVertical: false, start: -25, end: 100, step: 25, scaleStep: 10, hasTagmark: true, prefix: '%', color: 'red', from: null, to: 0},
-};
-
-arr[1] = {
-  options : {start: 250, end: 100, step: 300, scaleStep: 1000},
-  result : {isRange: false, start: 0, end: 100, step: 1, hasTagmark: true, scaleStep: 100, color: '#53b6a8', isVertical: false, prefix: null, from: null, to: 0},
-};
-
-arr[2] = {
-  options : {isRange: true, start: 250, end: 100, values: ['one', 'two', 'three', 4], isVertical: true, from: 'one', to: 'two'},
-  result : {isRange: true, isVertical: true, values: ['one', 'two', 'three', 4], hasTagmark: true, color: '#53b6a8', prefix: null, from: 'one', to: 'two'},
-};
-
-arr[3] = {
-  options : {isRange: false, start: 250, end: 100, values: ['one', 'two', 'three', 4], isVertical: true, hasTagmark: false, color: 'red', to: 'three'},
-  result : {isRange: false, isVertical: true, values: ['one', 'two', 'three', 4], color: 'red', hasTagmark: false, prefix: null, from: null, to: 'three'},
-};
-
-arr[4] = {
-  options : {isRange: true, start: 250, end: 100, scaleStep: 1000, from: 0, to: 50},
-  result : {isRange: true, start: 0, end: 100, step: 1, hasTagmark: true, scaleStep: 100, color: '#53b6a8', isVertical: false, prefix: null, from: 0, to: 50},
-};
-
-arr[5] = {
-  options : {isRange: true, start: 250, end: 100, values: ['one', 'two', 'three', 4], isVertical: true},
-  result : {isRange: true, isVertical: true, values: ['one', 'two', 'three', 4], hasTagmark: true, color: '#53b6a8', prefix: null, from: 'one', to: 'two'},
-};
-arr[6] = {
-  options : {isRange: true, start: 250, end: 100, scaleStep: 1000, from: 100},
-  result : {isRange: true, start: 0, end: 100, step: 1, hasTagmark: true, scaleStep: 100, color: '#53b6a8', isVertical: false, prefix: null, from: 0, to: 1},
-};
-
-arr[7] = {
-  options : {isRange: false, start: 250, end: 100, values: ['one', 'two', 'three', 4], isVertical: true, hasTagmark: false, color: 'red', },
-  result : {isRange: false, isVertical: true, values: ['one', 'two', 'three', 4], color: 'red', hasTagmark: false, prefix: null, from: null, to: 'two'},
-};
-
-arr.forEach((el) => {
-  const model = new Model(el.options);
-  const options = model.getOptions();
+  beforeEach(() => {
+    model.createOptions({});
+  });
 
   test('Set range', () => {
-    expect(options.isRange).toBe(el.result.isRange);
+    expect(model.getOptions().isRange).toBe(false);
+
+    model.createOptions({isRange: false});
+    expect(model.getOptions().isRange).toBe(false);
+
+    model.createOptions({isRange: true});
+    expect(model.getOptions().isRange).toBe(true);
   });
 
   test('Set direction', () => {
-    expect(options.isVertical).toBe(el.result.isVertical);
+    expect(model.getOptions().isVertical).toBe(false);
+
+    model.createOptions({isVertical: false});
+    expect(model.getOptions().isVertical).toBe(false);
+
+    model.createOptions({isVertical: true});
+    expect(model.getOptions().isVertical).toBe(true);
   });
 
   test('Set color', () => {
-    expect(options.color).toBe(el.result.color);
+    expect(model.getOptions().color).toBe('#53b6a8');
+
+    model.createOptions({color: 'purple'});
+    expect(model.getOptions().color).toBe('purple');
+
+    model.createOptions({color: '#eee'});
+    expect(model.getOptions().color).toBe('#eee');
   });
 
   test('Has tagmark', () => {
-    expect(options.hasTagmark).toBe(el.result.hasTagmark);
+    expect(model.getOptions().hasTagmark).toBe(true);
+
+    model.createOptions({hasTagmark: false});
+    expect(model.getOptions().hasTagmark).toBe(false);
+
+    model.createOptions({hasTagmark: true});
+    expect(model.getOptions().hasTagmark).toBe(true);
   });
 
   test('Min value of slider', () => {
-    expect(options.start).toBe(el.result.start);
+    expect(model.getOptions().start).toBe(0);
+
+    model.createOptions({start: 10, end: 50});
+    expect(model.getOptions().start).toBe(10);
+
+    model.createOptions({start: 100, end: 50});
+    expect(model.getOptions().start).toBe(0);
   });
 
   test('Max value of slider', () => {
-    expect(options.end).toBe(el.result.end);
+    expect(model.getOptions().end).toBe(100);
+
+    model.createOptions({start: 10, end: 50});
+    expect(model.getOptions().end).toBe(50);
+
+    model.createOptions({start: 100, end: 50});
+    expect(model.getOptions().end).toBe(100);
   });
 
   test('Set scaleStep', () => {
-    expect(options.scaleStep).toBe(el.result.scaleStep);
+    expect(model.getOptions().scaleStep).toBe(100);
+
+    model.createOptions({start: 10, end: 50});
+    expect(model.getOptions().scaleStep).toBe(40);
+
+    model.createOptions({start: 0, end: 50, scaleStep: 10});
+    expect(model.getOptions().scaleStep).toBe(10);
   });
 
   test('Set step', () => {
-    expect(options.step).toBe(el.result.step);
+    expect(model.getOptions().step).toBe(1);
+
+    model.createOptions({start: 10, end: 50, step: 80});
+    expect(model.getOptions().step).toBe(1);
+
+    model.createOptions({start: 0, end: 50, step: 8});
+    expect(model.getOptions().step).toBe(8);
   });
 
   test('Set prefix', () => {
-    expect(options.prefix).toBe(el.result.prefix);
+    expect(model.getOptions().prefix).toBe(null);
+
+    model.createOptions({prefix:'$'});
+    expect(model.getOptions().prefix).toBe('$');
   });
 
   test('Set values, if it is value slider', () => {
-    expect(options.values).toEqual(el.result.values);
+    expect(model.getOptions().values).toBe(undefined);
+
+    model.createOptions({values: ['one']});
+    expect(model.getOptions().values).toBe(undefined);
+
+    model.createOptions({values: ['one', 'two', 'three']});
+    expect(model.getOptions().values).toStrictEqual(['one', 'two', 'three']);
   });
 
   test('Set "from"-value for handle', () => {
-    expect(options.from).toBe(el.result.from);
+    expect(model.getOptions().from).toBe(null);
+
+    model.createOptions({start: 10, end: 50, isRange: true});
+    expect(model.getOptions().from).toBe(10);
+
+    model.createOptions({start: 10, end: 50, isRange: true, from: 20, to: 30});
+    expect(model.getOptions().from).toBe(20);
+
+    model.createOptions({start: 10, end: 50, isRange: true, from: 55, to: 30});
+    expect(model.getOptions().from).toBe(10);
+
+    model.createOptions({values: ['one', 'two', 'three', 'four', 'five'], isRange: true, from: 'three'});
+    expect(model.getOptions().from).toBe('three');
   });
 
   test('Set "to"-value for handle', () => {
-    expect(options.to).toBe(el.result.to);
+    expect(model.getOptions().to).toBe(0);
+
+    model.createOptions({start: 10, end: 50, isRange: true});
+    expect(model.getOptions().to).toBe(11);
+
+    model.createOptions({start: 10, end: 50, isRange: true, from: 20, to: 30});
+    expect(model.getOptions().to).toBe(30);
+
+    model.createOptions({start: 10, end: 50, isRange: true, from: 55, to: 30});
+    expect(model.getOptions().to).toBe(30);
+
+    model.createOptions({start: 10, end: 50, isRange: true, step: 5});
+    expect(model.getOptions().to).toBe(15);
+
+    model.createOptions({start: 10, end: 50, isRange: false, to: 30});
+    expect(model.getOptions().to).toBe(30);
+
+    model.createOptions({values: ['one', 'two', 'three', 'four', 'five'], isRange: true, to: 'three'});
+    expect(model.getOptions().to).toBe('three');
+
+    model.createOptions({values: ['one', 'two', 'three', 'four', 'five'], isRange: true, to: 'ten'});
+    expect(model.getOptions().to).toBe('two');
+
+    model.createOptions({values: ['one', 'two', 'three', 'four', 'five'], isRange: false, to: 'five'});
+    expect(model.getOptions().to).toBe('five');
   });
 });
