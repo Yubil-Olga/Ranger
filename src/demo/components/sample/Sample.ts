82c5d2b44ca2b3a8bc2db3df9ca71852fa378e26
Yubil-Olga <Yubil-Olga@yandex.ru>
2020-12-14 10:43:25 +0300

    names

---
+++ src/demo/components/sample/Sample.ts
@@ -0,0 +1,25 @@
+import Slider from '../slider/Slider';
+import ControlPanel from '../control-panel/ControlPanel';
+import { IOptions } from '../../../app/IOptions';
+
+export default class Sample {
+  public $sample: JQuery<Object>;
+  public settings: IOptions;
+  public slider: Slider;
+  public controlPanel: ControlPanel;
+
+  constructor($container: JQuery<Object>, settings?: IOptions) {
+    this.settings = settings;
+    this.findHTMLElement($container);
+    this.initSlider();
+  }
+
+  findHTMLElement($container: JQuery<Object>) {
+    this.$sample = $container.find('.js-sample');
+  }
+
+  initSlider() {
+    this.slider = new Slider(this.$sample, this.settings);
+    this.controlPanel = new ControlPanel(this.$sample, this.slider);
+  }
+}
